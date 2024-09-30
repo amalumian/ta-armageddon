d25bb66918a5518424830c6fcf09b204f2819b03
Arthur Malumian <amalumian.dev@gmail.com>
2024-10-01 04:23:12 +0700

    Add asteroid.list.tsx

---
+++ app/ui/asteroid-list.tsx
@@ -0,0 +1,58 @@
+import {
+  formatDate,
+  formatDiameterInM,
+  formatDistanceInKm,
+  formatDistanceInLunar,
+} from '@/app/lib/utils';
+import AsteroidItem from './asteroid-item';
+
+import { AsteroidsType } from '@/app/lib/definitions';
+
+type AsteroidListProps = {
+  asteroids: AsteroidsType;
+  distanceUnit: 'km' | 'lunar';
+};
+
+export default function AsteroidList({
+  distanceUnit,
+  asteroids,
+}: AsteroidListProps) {
+  return asteroids.map((obj) =>
+    obj.data.map((asteroid) => {
+      const date = formatDate(obj.date);
+
+      const distance = {
+        km: formatDistanceInKm(
+          asteroid.close_approach_data[0].miss_distance.kilometers,
+        ),
+        lunar: formatDistanceInLunar(
+          asteroid.close_approach_data[0].miss_distance.lunar,
+        ),
+      };
+
+      const name = asteroid.name.match(/\(([^)]+)\)/)![1];
+
+      const diameterMin: number =
+        asteroid.estimated_diameter.meters.estimated_diameter_min;
+      const diameterMax: number =
+        asteroid.estimated_diameter.meters.estimated_diameter_max;
+      const diameterAverage = formatDiameterInM(
+        (diameterMax + diameterMin) / 2,
+      );
+
+      const isHazardous = asteroid.is_potentially_hazardous_asteroid;
+
+      return (
+        <AsteroidItem
+          key={asteroid.id}
+          date={date}
+          distance={distance[distanceUnit]}
+          id={asteroid.id}
+          diameter={diameterAverage}
+          isHazardous={isHazardous}
+          name={name}
+        />
+      );
+    }),
+  );
+}
