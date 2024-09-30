f099e6607b45083273ef43f0950d20c53d449e78
Arthur Malumian <amalumian.dev@gmail.com>
2024-10-01 04:21:44 +0700

    Refactor root layout and main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next';
+import Image from 'next/image';
 
+import { passionOne } from '@/app/ui/fonts';
 import '@/app/globals.css';
+import styles from '@/app/layout.module.css';
+import Link from 'next/link';
+import { CartProvider } from './lib/context/cart-context';
 
 export const metadata: Metadata = {
   title: 'ARMAGEDDON',
@@ -13,8 +18,34 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru">
-      <body>{children}</body>
-    </html>
+    <CartProvider>
+      <html lang="ru">
+        <body>
+          <header className={styles.header}>
+            <h1 className={`${passionOne.className} ${styles.id}`} lang="en">
+              <Link href="/">ARMAGEDDON</Link>
+            </h1>
+            <p>ООО “Команда им. Б. Уиллиса”.</p>
+            <p>Взрываем астероиды с 1998 года.</p>
+          </header>
+          <main className={styles.main}>
+            {children}
+            <div className={styles['logo-container']}>
+              <Image
+                className={styles.logo}
+                src="/earth.webp"
+                alt="Earth locating on the left path of content"
+                width={1450}
+                height={1450}
+                priority
+              />
+            </div>
+          </main>
+          <footer className={styles.footer}>
+            © Все права и планета защищены
+          </footer>
+        </body>
+      </html>
+    </CartProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,18 @@
-import Image from 'next/image';
-
 import styles from '@/app/page.module.css';
-import { passionOne } from '@/app/ui/fonts';
 
-export default function Home() {
+import Asteroids from './ui/asteroids';
+import { Suspense } from 'react';
+import { fetchAsteroids } from './lib/data';
+
+export default async function Page() {
+  const { asteroids, asteroidCount } = await fetchAsteroids();
+
   return (
-    <div className={styles.page}>
-      <header className={styles.header}>
-        <h1 className={`${passionOne.className} ${styles.id}`} lang="en">
-          ARMAGEDDON
-        </h1>
-        <p>ООО “Команда им. Б. Уиллиса”.</p>
-        <p>Взрываем астероиды с 1998 года.</p>
-      </header>
-      <main className={styles.main}>
-        <h2>Ближайшие подлёты астероидов</h2>
-        <div>Block for filter</div>
-        <div>Block for list</div>
-        <div>Block for cart</div>
-        <div className={styles['logo-container']}>
-          <Image
-            className={styles.logo}
-            src="/earth.webp"
-            alt="Earth locating on the left path of content"
-            width={1450}
-            height={1450}
-          />
-        </div>
-      </main>
-      <footer className={styles.footer}>© Все права и планета защищены</footer>
-    </div>
+    <>
+      <h2>Ближайшие подлёты астероидов</h2>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Asteroids asteroids={asteroids} asteroidCount={asteroidCount} />
+      </Suspense>
+    </>
   );
 }
