1f10bca5be861b5f932deacd4ba038d3bbc813c3
Arthur Malumian <amalumian.dev@gmail.com>
2024-10-21 23:11:23 +0700

    Update loader state on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import styles from '@/app/page.module.css';
-
-import Asteroids from './ui/asteroids';
-import { Suspense } from 'react';
-import { fetchAsteroids } from './lib/data';
+import Asteroids from './ui/asteroids'
+import { fetchAsteroids } from './lib/data'
 
 export default async function Page() {
-  const { asteroids, asteroidCount } = await fetchAsteroids();
+  const { asteroids } = await fetchAsteroids()
 
   return (
     <>
       <h2>Ближайшие подлёты астероидов</h2>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Asteroids asteroids={asteroids} asteroidCount={asteroidCount} />
-      </Suspense>
+      <Asteroids asteroids={asteroids} />
     </>
-  );
+  )
 }
