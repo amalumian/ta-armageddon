40968aa178b6ca5b1eff3bc817cdc4bb8918da03
Arthur Malumian <amalumian.dev@gmail.com>
2024-10-24 22:50:38 +0700

    Style asteroid page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { fetchAsteroid } from '../lib/data'
 import { AsteroidType } from '../lib/definitions'
 import { formatDate } from '../lib/utils'
+import styles from './page.module.css'
 
 export default async function Page({ params }: { params: { id: string } }) {
   const asteroid: AsteroidType = await fetchAsteroid(params.id)
@@ -11,24 +12,31 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <>
       <h2>{asteroid.name}</h2>
-      <h3>Сближения</h3>
-      <div>Орбита: Земля</div>
-      <div>
+      <h3>Сближения (орбита: Земля)</h3>
+      <div className={styles.approaches}>
         {earthApproachData.map((approach) => {
           const approachDate = formatDate(approach.close_approach_date)
 
           return (
-            <div key={approach.close_approach_date}>
-              <div>Скорость: {approach.relative_velocity.kilometers_per_second} км/с</div>
-              <div>Время максимального сближения: {approachDate}</div>
-              <div>
-                Расстояние:
-                <ul>
-                  <li>В километрах: {approach.miss_distance.kilometers}</li>
-                  <li>В лунных орбитах: {approach.miss_distance.lunar}</li>
+            <ul className={styles.approaches__list} key={approach.close_approach_date}>
+              <li className={styles.approaches__item}>
+                <b>Скорость:</b> {approach.relative_velocity.kilometers_per_second} км/с
+              </li>
+              <li className={styles.approaches__item}>
+                <b>Время максимального сближения:</b> {approachDate}
+              </li>
+              <li className={styles.approaches__item}>
+                <b>Расстояние:</b>
+                <ul className={styles['approaches__distance-list']}>
+                  <li className={styles['approaches__distance-item']}>
+                    <b>В километрах:</b> {approach.miss_distance.kilometers}
+                  </li>
+                  <li className={styles['approaches__distance-item']}>
+                    <b>В лунных орбитах:</b> {approach.miss_distance.lunar}
+                  </li>
                 </ul>
-              </div>
-            </div>
+              </li>
+            </ul>
           )
         })}
       </div>
